d0539ae08d937fe7b4b7287670563963e01fa590
Israel Ribeiro <208321321+IsraelRibeiro01@users.noreply.github.com>
2025-06-06 20:03:43 -0300

    Flake branch change

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
    description = "Nix Flake";
 
    inputs = {
-     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs = {self, nixpkgs, ...}@inputs: {
