4ad13b322b3e0c9c5d2c2f615f9c722518d9b822
Israel Ribeiro <208321321+IsraelRibeiro01@users.noreply.github.com>
2025-06-06 21:21:26 -0300

    Flake 0.1

--- flake.nix
+++ flake.nix
@@ -3,14 +3,33 @@
 
    inputs = {
      nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+     home-manager.url = "github:nix-community/home-manager/release-25.05";
+     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {self, nixpkgs, ...}@inputs: {
-  nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    modules = [
+  outputs = {self, nixpkgs, home-manager, ...}@inputs:
+	let
+	  system = "x86_64-linux";
+	  hostname = "nixos";
+        in {
+      nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = { inherit inputs; };
+
+      modules = [
       ./configuration.nix
-      ];
-    };
-   };
- }
+      home-manager.nixosModules.home-manager
+      {
+      home-manager = {
+        useGlobalPkgs = true;
+	useUserPackages = true;
+	extraSpecialArgs = { inherit inputs; };
+	users.gaku = {
+	imports = [ ./home.nix ];
+	  };
+	};
+      }    
+    ];
+  };
+ };
+} 
